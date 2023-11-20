5c1576a909dcdb60a53220341c0a01493cd306ac
Erick Alejandro Vazquez Vargas <evazquez@pai.lat>
2023-11-20 00:26:08 -0600

    Agrega formulario

--- src/components/forms/Contact.vue
+++ src/components/forms/Contact.vue
@@ -4,7 +4,7 @@
 </script>
 <template>
     <section class="mx-5">
-        <div class="bg-website-white max-w-4xl mx-auto rounded-2xl p-10">
+        <!-- <div class="bg-website-white max-w-4xl mx-auto rounded-2xl p-10">
             <h2 class="text-2xl text-center">¿Quieres potenciar el alcance de tu negocio turístico?</h2>
             <p class="text-center pt-2">Déjanos tus datos y nos pondremos en contacto contigo</p>
             <form class="grid grid-cols-2 gap-5 mt-10">
@@ -14,6 +14,9 @@
                 <textarea class="border-website-gray border-2 rounded-xl p-2 col-span-2 md:col-auto" placeholder="Mensaje"></textarea>
                 <button class="bg-website-pink text-website-white rounded-xl p-2 col-span-2">Enviar</button>
             </form>
+        </div> -->
+        <div class="max-w-3xl bg-white rounded-2xl p-10 mx-auto">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRBs0gTbHt1mVRjzJYRlxOkiq4YNZYZfw_vLrASHFNCUb3Vw/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </div>
     </section>
 </template>
